41e0affabfa11bdcd202930da2e47c28a3e77b35
unknown <kautsar@live.fr>
2023-08-29 15:21:05 +0700

    test construct url

--- services/http-services.ts
+++ services/http-services.ts
@@ -1,3 +1,5 @@
+import apiUrl from "./api-url";
+
 class HttpService {
   endpoint: string;
 
@@ -8,7 +10,24 @@ class HttpService {
       "?key=" +
       process.env.NEXT_PUBLIC_RAWG_API_KEY;
 
-    this.endpoint = url;
+    console.log("httpservices.endpoint", url);
+
+    // ---------------
+    const urla = new URL(process.env.NEXT_PUBLIC_RAWG_API_URL!);
+    urla.search = new URLSearchParams({
+      key: process.env.NEXT_PUBLIC_RAWG_API_KEY!,
+    }).toString();
+    urla.pathname = urla.pathname.concat(term);
+    console.log("urla---> ", urla.href);
+    // ---------------
+    // apiUrl bermasalah karena import
+    // tiap rendre akant terjadi penambahan pathname
+    // /term/term/term/term?key=abc dan seterusnya
+    apiUrl.pathname = apiUrl.pathname.concat(term);
+    console.log("apiUrl:::->\n", apiUrl.toString());
+
+    // ---------------
+    this.endpoint = urla.href;
   }
 
   getAll() {
