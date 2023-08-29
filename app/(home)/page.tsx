bb64616dab58c0ea00a510b5ed21c957879a0338
unknown <kautsar@live.fr>
2023-08-29 13:46:23 +0700

    custom hook fetch game

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,31 +1,9 @@
 "use client";
-import { useEffect, useState } from "react";
-import httpServices from "@/services/http-services";
-import Link from "next/link";
-import { TGames } from "@/components/Types";
 import GridCard from "@/components/GridCard";
+import useGames from "@/hooks/useGames";
 
 const HomePage = () => {
-  const [games, setGames] = useState<TGames[]>([]);
-  const [isLoading, setLoading] = useState(true);
-  const [fetchError, setFetchError] = useState("");
-  useEffect(() => {
-    const fetchGames = httpServices("/games");
-    const { request, cancel } = fetchGames.getAll();
-    request
-      .then((res) => res.json())
-      .then((data) => {
-        setGames(data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err.name === "AbortError") return;
-        setFetchError(err.message);
-        setLoading(false);
-      });
-
-    return cancel;
-  }, []);
+  const { games, isLoading, fetchError } = useGames();
   return (
     <>
       <div>
---
+++ hooks/useGames.ts
@@ -0,0 +1,29 @@
+import { TGames } from "@/components/Types";
+import httpServices from "@/services/http-services";
+import React, { useEffect, useState } from "react";
+
+const useGames = () => {
+  const [games, setGames] = useState<TGames[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const [fetchError, setFetchError] = useState("");
+  useEffect(() => {
+    const fetchGames = httpServices("/games");
+    const { request, cancel } = fetchGames.getAll();
+    request
+      .then((res) => res.json())
+      .then((data) => {
+        setGames(data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") return;
+        setFetchError(err.message);
+        setLoading(false);
+      });
+
+    return cancel;
+  }, []);
+  return { games, isLoading, fetchError };
+};
+
+export default useGames;
