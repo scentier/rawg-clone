dd5fd32a789183dc8cb4126e2153417388201eac
unknown <kautsar@live.fr>
2023-09-05 08:17:23 +0700

    pakai service

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -1,54 +1,21 @@
-"use client";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import getGenres from "@/services/get-genres";
 import Link from "next/link";
 
-type TGenre = {
-  id: number;
-  name: string;
-};
-
-const Aside = () => {
-  const [gameGenres, setGameGenres] = useState<TGenre[]>([]);
-  const [isLoading, setLoading] = useState(true);
-  const [fetchError, setFetchError] = useState("");
-
-  useEffect(() => {
-    axios
-      .create({
-        baseURL: process.env.NEXT_PUBLIC_RAWG_API_URL,
-        params: {
-          key: process.env.NEXT_PUBLIC_RAWG_API_KEY,
-        },
-      })
-      .get("/genres")
-      .then((res) => {
-        setGameGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setFetchError(err.name + ": " + err.message);
-        setLoading(false);
-      });
-  }, []);
+export default async function Aside() {
+  const gameGenres = await getGenres();
 
   return (
     <div className="hidden w-1/6 float-left md:block">
-      <p className="text-red-400">{fetchError}</p>
       <ul className="mr-2 mt-4">
-        {isLoading
-          ? "Loading..."
-          : gameGenres.map((genre) => (
-              <li
-                className="p-1 mt-1 border-b-neutral-100 border-b hover:text-orange-500"
-                key={genre.id}
-              >
-                <Link href={`/genres/${genre.id}`}>{genre.name}</Link>
-              </li>
-            ))}
+        {gameGenres.map((genre) => (
+          <li
+            key={genre.id}
+            className="p-1 mt-1 border-b-neutral-100 border-b hover:text-orange-500"
+          >
+            <Link href={`/genres/${genre.id}/${genre.slug}`}>{genre.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
-};
-
-export default Aside;
+}
